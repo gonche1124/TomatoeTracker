d7d2e839ea657fbbfc45824171e81e40b877ef54
Andres Gonzalez <gonche1124@aol.com>
2021-01-06 20:30:34 -0500

    - Initial Setup

--- TomatoeTracker/CircularTimer.swift
+++ TomatoeTracker/CircularTimer.swift
@@ -13,13 +13,37 @@ struct CircularTimer: View {
     var lineColor:Color = .green
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle()
+                .stroke(Color.gray, lineWidth: 15)
+               
+            Circle()
+                .trim(from: 0, to: CGFloat(fractionFull))
+                .stroke(style:innerCircleStyle )
+        }
+        .frame(width: 250, height: 250)
+    }
+    
+    var isCompleted:Bool{
+        fractionFull >= 1.0
+    }
+    
+    var innerCircleStyle:StrokeStyle{
+        StrokeStyle(lineWidth: 15, lineCap: .round, lineJoin: .round)
     }
 }
 
 struct CircularTimer_Previews: PreviewProvider {
     static var previews: some View {
-        CircularTimer()
-            .padding()
+        Group{
+            CircularTimer()
+                .padding()
+                .colorScheme(.dark)
+            
+            CircularTimer()
+                .padding()
+                .colorScheme(.light)
+        }
+        
     }
 }
